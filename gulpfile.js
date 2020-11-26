1b485641f4803c8ecec785c6dbea8d0a73867305
Tanya <krivorotovatd@gmail.com>
2020-11-26 17:19:11 +0300

    slider

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,28 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const babel = require("gulp-babel");
 
 sass.compiler = require("node-sass");
+
+function build() {
+    return gulp
+        .src("./js/*.js")
+        .pipe(
+            babel({
+                presets: ["@babel/env"],
+            })
+        )
+        .pipe(uglify({ mangle: { toplevel: true } }))
+        .pipe(
+            rename((path) => {
+                path.basename += "-min";
+            })
+        )
+        .pipe(gulp.dest("dist"));
+}
 
 function style() {
     return gulp
@@ -24,8 +44,9 @@ function watch() {
     });
     gulp.watch("./scss/**/*.scss", style);
     gulp.watch("./*.html").on("change", browserSync.reload);
-    gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+    gulp.watch("./js/*.js").on("change", browserSync.reload);
 }
 
 exports.style = style;
 exports.watch = watch;
+exports.build = build;
