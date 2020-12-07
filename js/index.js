48c86689b4f81fe4eab5c10c097807d5ea18281e
Timur Garipov <tbgaripov@gmail.com>
2020-12-07 12:45:07 +0300

    Дебаг мобильное меню + кнопка вверх

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const headerButtonsContainer = document.getElementsByClassName(
 const sideMenuContainer = document.getElementsByClassName("side-menu")[0];
 
 openButton.addEventListener("click", () => {
-    sideMenuContainer.style.display = "block";
+    sideMenuContainer.style.display = "flex";
     headerButtonsContainer.style.display = "none";
 });
 
@@ -16,3 +16,20 @@ closeButton.addEventListener("click", () => {
     sideMenuContainer.style.display = "none";
     headerButtonsContainer.style.display = "flex";
 });
+
+
+// Плавный переход к якорю
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
